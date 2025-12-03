34b0bea2aca1dce448ac9fbeeb053fb28d329d2e
kietnnn <kietnn@gmail.com>
2025-12-04 04:53:37 +0700

    1

---
+++ src/main/java/com/northwind/Program.java
@@ -0,0 +1,31 @@
+package com.northwind;
+
+import com.northwind.data.CustomerDao;
+import com.northwind.model.Customer;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.util.List;
+
+
+public class Program {
+    public static void main(String[] args) {
+
+
+        String username = args[0];
+        String password = args[1];
+        String url = "jdbc:mysql://localhost:3306/Northwind";
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        CustomerDao customerDao =new CustomerDao(dataSource);
+
+        List<Customer> customers =  customerDao.getAll();
+        System.out.println(customers);
+
+
+
+    }
+}
---
+++ src/main/java/com/northwind/data/CustomerDao.java
@@ -0,0 +1,61 @@
+package com.northwind.data;
+
+import com.northwind.model.Customer;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerDao {
+    private DataSource dataSource;
+
+    public CustomerDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<Customer> getAll(){
+        List<Customer> customers = new ArrayList<>();
+
+        String query = """
+                SELECT CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax
+                FROM Customers;
+                """;
+
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+
+                while (resultSet.next()) {
+                    Customer customer = new Customer(
+                            resultSet.getString("CustomerID"),
+                            resultSet.getString("CompanyName"),
+                            resultSet.getString("ContactName"),
+                            resultSet.getString("ContactTitle"),
+                            resultSet.getString("Address"),
+                            resultSet.getString("City"),
+                            resultSet.getString("Region"),
+                            resultSet.getString("PostalCode"),
+                            resultSet.getString("Country"),
+                            resultSet.getString("Phone"),
+                            resultSet.getString("Fax"));
+
+                    customers.add(customer);
+                }
+
+            }
+        } catch (SQLException e) {
+            System.out.println("There was an error retrieving the data. Please try again.");
+            e.printStackTrace();
+        }
+
+        return customers;
+    }
+
+
+}
